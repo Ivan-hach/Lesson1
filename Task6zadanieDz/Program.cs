88d947ebd2c77f685434dabad757f9bbc3d28808
Ivan Taigin <vanya-tajgin@yandex.ru>
2024-02-15 15:47:51 +0300

    add Task6Dz

---
+++ Task6zadanieDz/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 6;
+int b = 12;
+int c = 4;
+int d = 20;
+int sum = a * b;
+int sum2 = c + d;
+int sum3 = sum / sum2;
+
+int product1 = a * b;
+int product2 = c + d;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+
+int quotient = sum / sum2;
+Console.WriteLine(quotient);
